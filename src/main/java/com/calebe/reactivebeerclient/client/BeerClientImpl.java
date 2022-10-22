a77aa7dfc9d79cfb9cc0aafb01292c097dc01f51
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2022-10-22 15:33:04 -0300

    implement parameters in findById methods on BeerClientImpl

--- src/main/java/com/calebe/reactivebeerclient/client/BeerClientImpl.java
+++ src/main/java/com/calebe/reactivebeerclient/client/BeerClientImpl.java
@@ -19,10 +19,9 @@ public class BeerClientImpl implements BeerClient {
     private final WebClient webClient;
     @Override
     public Mono<BeerDto> getBeerById(UUID id, Boolean showInventoryOnHand) {
-        return webClient.get().uri(uriBuilder -> uriBuilder.path(WebClientProperties.BEER_V1_PATH +
-                "/" + id.toString())
+        return webClient.get().uri(uriBuilder -> uriBuilder.path(WebClientProperties.BEER_V1_PATH_GET_BY_ID )
                         .queryParamIfPresent("showInventoryOnHand", Optional.ofNullable(showInventoryOnHand))
-                .build()
+                .build(id)
         ).retrieve()
                 .bodyToMono(BeerDto.class);
     }
@@ -59,8 +58,8 @@ public class BeerClientImpl implements BeerClient {
     @Override
     public Mono<BeerDto> getBeerByUPC(String upc) {
         return webClient.get()
-                .uri(uriBuilder -> uriBuilder.path(WebClientProperties.BEER_V1_UPC_PATH + "/" + upc)
-                        .build())
+                .uri(uriBuilder -> uriBuilder.path(WebClientProperties.BEER_V1_UPC_PATH)
+                        .build(upc))
                 .retrieve()
                 .bodyToMono(BeerDto.class);
     }
